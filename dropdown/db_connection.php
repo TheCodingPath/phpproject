fd81eadef49099d86c0fbaac6e8b8bf5973be955
TheCodingPath <studio@thecodingpath.com>
2024-08-17 16:42:30 +0500

    Update db_connection.php

--- dropdown/db_connection.php
+++ dropdown/db_connection.php
@@ -1,7 +1,7 @@
 <?php $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "country_database";
+$dbname = "geo_data";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
